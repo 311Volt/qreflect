ee42843776545f4485a3326864f7fdd189d9bb74
311Volt <113amper@gmail.com>
2024-02-26 20:33:58 +0100

    initial macroless version

--- examples/example.cpp
+++ examples/example.cpp
@@ -1,54 +1,52 @@
 #include <qreflect.hpp>
 
 #include <iostream>
-#include <stdint.h>
 #include <string>
-#include <sstream>
 
 struct Consents {
 	bool emailContact = false;
 	bool phoneContact = false;
+
+	static constexpr std::tuple qrefl_fields = {
+		&Consents::emailContact,
+		&Consents::phoneContact
+	};
 };
+static_assert(qreflect::has_field_info<Consents>);
 
 struct User {
 	std::string firstName;
 	std::string lastName;
 	int age;
 	Consents consents;
+
+	static constexpr std::tuple qrefl_fields = {
+		&User::firstName,
+		&User::lastName,
+		&User::age,
+		&User::consents
+	};
 };
-
-QREFL_DESCRIBE(Consents, emailContact, phoneContact)
-QREFL_DESCRIBE(User, firstName, lastName, age, consents)
-
-template<typename T>
-	requires requires(T v, std::ostream& os){os << v;}
-std::string to_str(T&& value, int indent = 0)
-{
-	std::stringstream ss;
-	ss << value;
-	return ss.str();
-}
-
-template<qreflect::inspectable T>
-std::string to_str(T&& value, int indent = 0)
-{
-	std::stringstream ss;
-    ss << "\n";
-	qreflect::for_each_member_of(value, [&](const std::string_view name, auto&& value){
-		ss << std::string(indent, ' ') << name << ": " << to_str(value, indent+4) << "\n";
-	});
-	return ss.str();
-}
+static_assert(qreflect::has_field_info<User>);
 
 int main() {
-	User exampleUser {
-		.firstName = "Gabe",
-		.lastName = "Itch",
-		.age = 27,
-		.consents = {
-			.emailContact = true,
-			.phoneContact = false
-		}
-	};
-	std::cout << to_str(exampleUser);
+	for(auto& s: qreflect::field_names_v<User>) {
+		std::cout << s << "\n";
+	}
+}
+
+
+int old_main() {
+	// TODO restore
+	// User exampleUser {
+	// 	.firstName = "Gabe",
+	// 	.lastName = "Itch",
+	// 	.age = 27,
+	// 	.consents = {
+	// 		.emailContact = true,
+	// 		.phoneContact = false
+	// 	}
+	// };
+	// std::cout << to_str(exampleUser);
+	return 0;
 }
--- include/qreflect.hpp
+++ include/qreflect.hpp
@@ -1,172 +1,286 @@
 #ifndef QREFLECT_QREFLECT_HPP
 #define QREFLECT_QREFLECT_HPP
 
+#include <algorithm>
+#include <array>
+#include <span>
 #include <concepts>
+#include <source_location>
 #include <string_view>
+#include <tuple>
 #include <utility>
+#include <vector>
 
-#ifndef QREFL_MAX_ARITY
-#define QREFL_MAX_ARITY 32
-#endif
+#define QREFL_ENABLE_ANNOTATIONS() \
+	protected: \
+		static constexpr size_t qreflect_counter_base = __COUNTER__; \
+		template<size_t N> static constexpr auto qreflect_annotation_val = ::qreflect::no_value{}; \
+		template<auto MemPtr> static constexpr auto qreflect_annotation = ::qreflect::no_value{}; \
+	public: \
+		using qreflect_annotation_enabled_tag = void;
 
-#define QREFL_STATIC_ASSERT_FALSE(T, msg) \
-	static_assert(requires(T x){{x.fq2358798c7f78q26hfc897hr6h6};}, msg)
+#define QREFL_INTERNAL_ANN_END(memptr, idx_) \
+	static_assert((idx_ - qreflect_counter_base) % 2 == 0, "QREFL_ANN_BEGIN / QREFL_ANN_END mismatch detected"); \
+	template<> static constexpr auto qreflect_annotation<memptr> = qreflect_annotation_val<idx_ - 1>;
 
-#define QREFL_PARENS ()
+#define QREFL_ANN_BEGIN() \
+	template<> static constexpr auto qreflect_annotation_val<__COUNTER__>
 
-#define QREFL_EXPAND(...) QREFL_EXPANDMAX(__VA_ARGS__)
-
-#if QREFL_MAX_ARITY > 512
-#error "QREFL_MAX_ARITY cannot exceed 512"
-#elif QREFL_MAX_ARITY > 1+256
-#define QREFL_EXPANDMAX QREFL_EXPAND8
-#elif QREFL_MAX_ARITY > 1+128
-#define QREFL_EXPANDMAX QREFL_EXPAND7
-#elif QREFL_MAX_ARITY > 1+64
-#define QREFL_EXPANDMAX QREFL_EXPAND6
-#elif QREFL_MAX_ARITY > 1+32
-#define QREFL_EXPANDMAX QREFL_EXPAND5
-#elif QREFL_MAX_ARITY > 1+16
-#define QREFL_EXPANDMAX QREFL_EXPAND4
-#elif QREFL_MAX_ARITY > 1+8
-#define QREFL_EXPANDMAX QREFL_EXPAND3
-#elif QREFL_MAX_ARITY > 1+4
-#define QREFL_EXPANDMAX QREFL_EXPAND2
-#elif QREFL_MAX_ARITY > 1+2
-#define QREFL_EXPANDMAX QREFL_EXPAND1
-#elif QREFL_MAX_ARITY == 1+2
-#define QREFL_EXPANDMAX QREFL_EXPAND0
-#else
-#error "QREFL_MAX_ARITY must be at least 3"
-#endif
-
-
-#define QREFL_EXPAND8(...) QREFL_EXPAND7(QREFL_EXPAND7(__VA_ARGS__))
-#define QREFL_EXPAND7(...) QREFL_EXPAND6(QREFL_EXPAND6(__VA_ARGS__))
-#define QREFL_EXPAND6(...) QREFL_EXPAND5(QREFL_EXPAND5(__VA_ARGS__))
-#define QREFL_EXPAND5(...) QREFL_EXPAND4(QREFL_EXPAND4(__VA_ARGS__))
-#define QREFL_EXPAND4(...) QREFL_EXPAND3(QREFL_EXPAND3(__VA_ARGS__))
-#define QREFL_EXPAND3(...) QREFL_EXPAND2(QREFL_EXPAND2(__VA_ARGS__))
-#define QREFL_EXPAND2(...) QREFL_EXPAND1(QREFL_EXPAND1(__VA_ARGS__))
-#define QREFL_EXPAND1(...) QREFL_EXPAND0(QREFL_EXPAND0(__VA_ARGS__))
-#define QREFL_EXPAND0(...) __VA_ARGS__
-
-#define QREFL_FOR_EACH(macro, type_name, counter, ...)                                    \
-	__VA_OPT__(QREFL_EXPAND(QREFL_FOR_EACH_HELPER(macro, type_name, counter, __VA_ARGS__)))
-#define QREFL_FOR_EACH_HELPER(macro, type_name, counter, a1, ...)                         \
-	macro(type_name, counter, a1)                                                     \
-	__VA_OPT__(QREFL_FOR_EACH_AGAIN QREFL_PARENS (macro, type_name, (counter+1), __VA_ARGS__))
-#define QREFL_FOR_EACH_AGAIN() QREFL_FOR_EACH_HELPER
-
-#define QREFL_CHECK_NUM_OF_FIELDS(type_name, ...) \
-	namespace qreflect {                             \
-        template<> \
-		void fail_compilation_if_num_of_fields_does_not_match(type_name&& qrefl_val) { \
-        	const auto& [__VA_ARGS__] = qrefl_val; \
-		} \
-	} \
-
-#define QREFL_DESCRIBE_FIELD(type_name, field_idx, field_name) \
-	namespace qreflect { template<> struct field_info<type_name, field_idx> { \
-        using parent_type = type_name; \
-    	using type = std::remove_cvref_t<decltype(type_name{}.field_name)>;   \
-        static inline constexpr int index = field_idx; \
-        static inline constexpr std::string_view name = #field_name; \
-		static inline const auto ptr_to_member = &type_name::field_name; \
-	};};
-
-#define QREFL_COUNT_ARGS_HELPER(dummy1, dummy2, dummy3) \
-	argcount++;
-
-#define QREFL_COUNT_ARGS(...) []{ \
-	int argcount = 0;                \
-    QREFL_FOR_EACH(QREFL_COUNT_ARGS_HELPER, 0, 0, __VA_ARGS__) \
-	return argcount; \
-}(); \
-
-
-#define QREFL_DESCRIBE_PARTIAL(type_name, ...) \
-	QREFL_FOR_EACH(QREFL_DESCRIBE_FIELD, type_name, 0, __VA_ARGS__) \
-	namespace qreflect {                          \
-        template<> constexpr inline bool is_inspectable_v<type_name> = true; \
-		template<> struct type_info<type_name> { \
-			using type = type_name; \
-			static inline constexpr int num_fields = QREFL_COUNT_ARGS(__VA_ARGS__) \
-			static inline constexpr std::string_view name = #type_name; \
-		};                                            \
-	};
-
-#define QREFL_DESCRIBE(type_name, ...) \
-    QREFL_DESCRIBE_PARTIAL(type_name, __VA_ARGS__) \
-    QREFL_CHECK_NUM_OF_FIELDS(type_name, __VA_ARGS__) \
-	static_assert(std::is_aggregate_v<type_name>, "Cannot fully reflect non-aggregate types");
-
+#define QREFL_ANN_END(memptr) \
+	QREFL_INTERNAL_ANN_END(memptr, __COUNTER__)
 
 
 namespace qreflect {
 
-	template<typename T, int N>
-	struct field_info {
-
-	};
+	struct no_value {};
 
 	template<typename T>
-	struct type_info {
-
-	};
-
-	template<typename TVal, typename TValAgain>
-	struct has_correct_num_fields: public std::false_type {
-		void dummy(TValAgain&& v) {}
-	};
+	inline constexpr auto fields_description = no_value {};
 
 	template<typename T>
-	void fail_compilation_if_num_of_fields_does_not_match(T&&) {
+	inline constexpr auto methods_description = no_value {};
 
+
+	namespace detail {
+
+		struct convertible_to_any {
+			template<typename T>
+			operator T();
+		};
+
+		template<typename T>
+		struct class_of_mem_ptr_helper {};
+
+		template<typename MemT, typename ClassT>
+		struct class_of_mem_ptr_helper<MemT ClassT::*> {
+			using MemberType = MemT;
+			using ClassType = ClassT;
+		};
+
+		template<typename T>
+		concept aggregate = std::is_aggregate_v<T>;
+
+		template<typename T>
+		concept member_object_ptr = std::is_member_object_pointer_v<T>;
+
+		template<typename T>
+		concept member_function_ptr = std::is_member_function_pointer_v<T>;
+
+		template<typename T>
+		concept member_ptr = std::is_member_pointer_v<T>;
+
+		template<member_ptr T>
+		using class_of_member_ptr = typename class_of_mem_ptr_helper<T>::ClassType;
+
+		enum class member_list_type_requirement {
+			none,
+			field,
+			method
+		};
+
+		template<typename TupleT>
+		struct tagged_member_list {
+			TupleT value {};
+			member_list_type_requirement require_type = member_list_type_requirement::field;
+			bool require_valid = true;
+			bool require_complete = false;
+			bool require_unique = false;
+		};
+
+		template<typename T>
+		consteval auto find_field_list() {
+			if constexpr(requires{T::qrefl_fields;}) {
+				return T::qrefl_fields;
+			} else if constexpr(not std::is_same_v<no_value, std::remove_cvref_t<decltype(fields_description<T>)>>) {
+				return fields_description<T>;
+			} else {
+				throw "no field description is available for this type";
+			}
+		}
+
+		template<typename T>
+		consteval auto find_method_list() {
+			if constexpr(requires{T::qrefl_methods;}) {
+				return T::qrefl_methods;
+			} else if constexpr(not std::is_same_v<no_value, std::remove_cvref_t<decltype(methods_description<T>)>>) {
+				return methods_description<T>;
+			} else {
+				throw "no method description is available for this type";
+			}
+		}
+
+		template<typename T>
+		struct field_list {
+			static constexpr auto value = find_field_list<T>();
+		};
+
+		template<typename T>
+		struct method_list {
+			static constexpr auto value = find_method_list<T>();
+		};
+
+		template<auto V>
+		constexpr const char* fn_name() {
+			return std::source_location::current().function_name();
+		}
+
+		template<typename T>
+		constexpr const char* fn_name() {
+			return std::source_location::current().function_name();
+		}
+
+		consteval std::string_view parse_member_name_gcc_clang(const std::string_view fnsig) {
+			size_t begin = fnsig.rfind("::") + 2;
+			size_t end = fnsig.find_last_not_of(" })]>");
+			return fnsig.substr(begin, 1+end-begin);
+		}
+
+		consteval std::string_view parse_type_name_gcc_clang(const std::string_view fnsig) {
+			size_t begin = fnsig.find("T = ") + 4;
+			size_t dcolon = fnsig.rfind("::");
+			if(dcolon > begin && dcolon != std::string_view::npos) {
+				begin = dcolon+2;
+			}
+			size_t end = fnsig.find_last_not_of(" })]>");
+			return fnsig.substr(begin, 1+end-begin);
+		}
+
+		template<typename T>
+		consteval std::string_view type_name() {
+			auto funcsig = fn_name<T>();
+			return parse_type_name_gcc_clang(funcsig);
+		}
+
+		template<size_t N>
+		constexpr bool has_no_duplicate_strs(std::array<std::string_view, N> strings) {
+			std::sort(strings.begin(), strings.end());
+			const auto new_end = std::unique(strings.begin(), strings.end());
+			return std::distance(strings.end(), new_end) == 0;
+		}
+
+		template<auto MemPtr>
+		consteval std::string_view member_name() {
+			auto funcsig = fn_name<MemPtr>();
+			return parse_member_name_gcc_clang(funcsig);
+		}
+
+		template<typename T, size_t... Idxs>
+		consteval auto member_names_helper(std::index_sequence<Idxs...>) {
+			return std::array {member_name<std::get<Idxs>(T::value)>()...};
+		}
+
+		template<typename ListT>
+		consteval auto member_names_impl() {
+			constexpr size_t len = std::tuple_size_v<decltype(ListT::value)>;
+			return member_names_helper<ListT>(std::make_index_sequence<len>{});
+		}
+
+		template<typename T>
+		consteval auto field_names() {
+			return member_names_impl<field_list<T>>();
+		}
+
+		template<typename T>
+		consteval auto method_names() {
+			return member_names_impl<method_list<T>>();
+		}
+
+
+
+		template<typename T, typename... Ms>
+		consteval bool is_member_list_valid(const std::tuple<Ms...>&) {
+			return (std::is_same_v<T, class_of_member_ptr<Ms>> && ...);
+		}
+
+		template<typename T, typename... Ms>
+		consteval bool is_member_list_field_list(const std::tuple<Ms...>&) {
+			return (member_object_ptr<Ms> && ...);
+		}
+
+		template<typename T, typename... Ms>
+		consteval bool is_member_list_method_list(const std::tuple<Ms...>&) {
+			return (member_function_ptr<Ms> && ...);
+		}
+
+		template<typename T, member_object_ptr... Ms>
+		consteval bool is_aggregate_field_list_complete_impl([[maybe_unused]] Ms... mem_ptrs)
+		{
+			return     requires(T v){T{v.*mem_ptrs...};}
+				&& not requires(T v){T{v.*mem_ptrs..., convertible_to_any{}};};
+		}
+
+		template<typename T, member_object_ptr... Ms>
+		consteval bool is_aggregate_field_list_complete(const std::tuple<Ms...>& mem_ptrs) {
+			return std::apply(is_aggregate_field_list_complete_impl<T, Ms...>, mem_ptrs);
+		}
+
+
+		template<typename T, typename ListT>
+		struct basic_member_list_info {
+			static constexpr auto member_list = ListT::value;
+			static constexpr auto member_name_list = member_names_impl<ListT>();
+
+			static constexpr bool is_valid = is_member_list_valid<T>(member_list);
+			static constexpr bool is_field_list = is_member_list_field_list<T>(member_list);
+			static constexpr bool is_method_list = is_member_list_method_list<T>(member_list);
+			static constexpr bool is_complete = is_aggregate_field_list_complete<T>(member_list);
+			static constexpr bool has_no_duplicates = has_no_duplicate_strs(member_name_list);
+		};
+
+		template<typename T>
+		using field_info = basic_member_list_info<T, field_list<T>>;
+
+		template<typename T>
+		using method_info = basic_member_list_info<T, method_list<T>>;
 	}
 
 	template<typename T>
-	inline constexpr bool has_correct_num_fields_v = has_correct_num_fields<T, T>::value;
+	concept has_field_info = requires
+	{
+		/* Description must be valid - it must be an std::tuple of pointers-to-members of T */
+		requires detail::field_info<T>::is_valid;
 
-	template<typename T>
-	concept field_info_type = requires {
-		typename T::parent_type;
-		typename T::type;
-		{T::index} -> std::convertible_to<int>;
-		{T::name} -> std::convertible_to<std::string_view>;
-		{T::ptr_to_member};
+		/* Description must be comprised entirely of pointers to data members (as opposed to methods) */
+		requires detail::field_info<T>::is_field_list;
+
+		/* If T is aggregate, the description must be complete, i.e. it must describe ALL of the aggregate's fields */
+		requires !std::is_aggregate_v<T> || detail::field_info<T>::is_complete;
+
+		/* If T is aggregate, the description must not have duplicate entries */
+		requires !std::is_aggregate_v<T> || detail::field_info<T>::has_no_duplicates;
 	};
 
 	template<typename T>
-	concept type_info_type = requires {
-		typename T::type;
-		{T::num_fields} -> std::convertible_to<int>;
-		{T::name} -> std::convertible_to<std::string_view>;
+	concept has_method_info = requires
+	{
+		/* Description must be valid - it must be an std::tuple of pointers-to-members of T */
+		requires detail::method_info<T>::is_valid;
+
+		/* Description must be comprised entirely of pointers to methods (as opposed to data members) */
+		requires detail::method_info<T>::is_method_list;
 	};
 
 	template<typename T>
-	constexpr inline bool is_inspectable_v = false;
+	inline constexpr auto field_list_v = detail::field_info<T>::member_list;
 
 	template<typename T>
-	concept inspectable = is_inspectable_v<std::remove_cvref_t<T>>;
+	inline constexpr auto method_list_v = detail::method_info<T>::member_list;
 
-	template<typename TData, typename TFn>
-		requires inspectable<std::remove_cvref_t<TData>>
-	void for_each_member_decl(TFn&& fn) {
-		using TDataV = std::remove_cvref_t<TData>;
-		[&]<size_t... Idxs>(std::index_sequence<Idxs...> seq) {
-			((fn.template operator()<field_info<TDataV, Idxs>>()), ...);
-		}(std::make_index_sequence<type_info<TDataV>::num_fields>{});
-	}
+	template<typename T>
+	inline constexpr auto field_names_v = detail::field_info<T>::member_name_list;
 
-	template<typename TData, typename TFn>
-		requires inspectable<std::remove_cvref_t<TData>>
-	void for_each_member_of(TData&& value, TFn&& fn) {
-		for_each_member_decl<TData>([&]<field_info_type FieldInfoT>(){
-			fn(FieldInfoT::name, value.*(FieldInfoT::ptr_to_member));
-		});
-	}
+	template<typename T>
+	inline constexpr auto method_names_v = detail::method_info<T>::member_name_list;
 
+	template<typename T>
+	inline constexpr auto type_name_v = detail::type_name<T>();
+
+	template<auto V>
+		requires detail::member_ptr<decltype(V)>
+	inline constexpr auto member_annotation_v = detail::class_of_member_ptr<decltype(V)>::template qreflect_annotation<V>;
+
+	template<typename T>
+	inline constexpr auto type_annotation_v = no_value{};
 
 }
 
